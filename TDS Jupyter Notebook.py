a50d067a9ff11ca1207a0f355af82ec2d2d9973f
Jova146 <140911753+Jova146@users.noreply.github.com>
2023-07-29 16:41:00 -0400

    Add files via upload

---
+++ TDS Jupyter Notebook.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ## Author
+# Jon Abbott
+
+# **Objectives:**
+# * List popular languages for Data Science
+# * List common libraries for Data Science
+# * Show basic arithmetic expressions
+
+# Some of the popular languages that Data Scientists use are:
+#    1. Python
+#    2. R
+#    3. Julia
+#    4. Ruby
+
+# Some of the commonly used libraries used by Data Scientists include:
+#    1. Pandas
+#    2. Seaborn
+#    3. Matplotlib
+
+# |Data Science Tools|
+# -----------------------------
+# |Apache AirFlow|
+# |Pixie Dust|
+# |Tensor Flow|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[5]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[11]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+(200/60)
+
+
+# In[ ]:
+
+
+
+
